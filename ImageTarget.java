91ca11e85d70e43abe5578cca825e2673133bf51
Carlos Xavier <carlosf.s.x@hotmail.com>
2019-10-29 21:57:41 -0300

    removendo classe renomeada

--- ImageTarget.java
+++
@@ -1,8 +0,0 @@
-
-import javax.swing.ImageIcon;
-
-public interface ImageTarget {
-	public ImageIcon carregarIcon(String nomeDoArquivo);
-
-	public void desenharImagem(int posX, int posY, int largura, int altura);
-}
